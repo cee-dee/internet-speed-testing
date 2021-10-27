41ec92c768f291df59eb1f61a98b733b2a6e4591
Bjoern Kopiske <bjoern.kopiske@check24.de>
2021-10-27 19:58:53 +0200

    Improve error handling by reporting states as result on completion without callbacks Report ongoing, successful and failed states Move repetion handling to caller to avoid average computation errors Clean up code Remove unused code Fix naming mixups

--- app/src/main/java/com/oatrice/internet_speed_testing/MainActivity.kt
+++ app/src/main/java/com/oatrice/internet_speed_testing/MainActivity.kt
@@ -4,7 +4,10 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.internet_speed_testing.DownloadSpeedMeasurement
 import com.example.internet_speed_testing.DownloadSpeedMeasurementBuilder
+import com.example.internet_speed_testing.FinalMeasurementState
+import com.example.internet_speed_testing.IntermediateMeasurementState
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
@@ -25,24 +28,59 @@ class MainActivity : AppCompatActivity() {
 
         adapter = Adapter()
         recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = adapter;
+        recyclerView.adapter = adapter
     }
 
     override fun onStart() {
         super.onStart()
-        val measurement = DownloadSpeedMeasurementBuilder()
-            .withUrl("https://leil.de/di/files/more/testdaten/1mb.test")
-            .withRepetitionCount(20)
-            .withProgressListener {
-                withContext(Dispatchers.Main) {
-                    adapter.updateDownload(it)
-                }
-            }
-            .build()
 
         MainScope().launch {
-            measurement.run()
+            (1..5).forEach {
+                val result = createMeasurementWithRecyclerViewUpdater(it)
+                    .run()
+                when (result) {
+                    is FinalMeasurementState.FinishedSuccessfully -> {
+                        result.overallBandwidth?.let { bandwidthResult ->
+                            adapter.updateDownload(
+                                it,
+                                bandwidthResult,
+                                FinishState.SUCCESS
+                            )
+                        }
+                    }
+                    is FinalMeasurementState.FinishedWithError -> {
+                        result.overallBandwidth?.let { bandwidthResult ->
+                            adapter.updateDownload(
+                                it,
+                                bandwidthResult,
+                                FinishState.ERROR
+                            )
+                        }
+                    }
+                }
+            }
         }
     }
 
+    private fun createMeasurementWithRecyclerViewUpdater(repetition: Int): DownloadSpeedMeasurement {
+
+        return createMeasurementExecutor {
+            adapter.updateDownload(
+                repetition,
+                it.overallBandwidth,
+                FinishState.NOT_FINISHED
+            )
+        }
+    }
+
+    private fun createMeasurementExecutor(listener: (IntermediateMeasurementState) -> Unit): DownloadSpeedMeasurement {
+        return DownloadSpeedMeasurementBuilder()
+            .withUrl("https://leil.de/di/files/more/testdaten/10mb.test")
+            .withProgressListener {
+                withContext(Dispatchers.Main) {
+                    listener(it)
+                }
+            }
+            .build()
+    }
 }
